456496737153bdcae8143a8f1fa1cd96bc48e07f
ericwright81 <wrighte3@carleton.edu>
2021-02-15 20:05:36 +0000

    Added config file

--- source/native.cpp
+++ source/native.cpp
@@ -1,9 +1,9 @@
 //  This file is part of Evolutionary Algorithm
 //  Copyright (C) Anya Vostinar, 2021.
 //  Released under MIT license; see LICENSE
-
+// run with make / ./evo-algo
 #include <iostream>
-
+#include "emp/config/ArgManager.hpp"
 #include "emp/base/vector.hpp"
 #include "emp/config/command_line.hpp"
 #include "emp/math/random_utils.hpp"
@@ -12,15 +12,25 @@
 #include "World.h"
 
 // This is the main function for the NATIVE version of this project.
+EMP_BUILD_CONFIG(MyConfigType,
+    VALUE(SEED, int, 10, "What value should the random seed be?"), 
+    VALUE(START_PROB, double, 0.5, "What cooperation probability value should the starting organism have?"),
+    VALUE(FILE_NAME, std::string, "_data.dat", "Root output file name")
+)
 
 int main(int argc, char* argv[])
 {
+  MyConfigType config;
+  config.Read("MySettings.cfg");
+  bool success = config.Read("MySettings.cfg");
+  if(!success) config.Write("MySettings.cfg");
+  
   emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
 
   emp::Random random(2);
   OrgWorld world(random);
 
-  //world.SetupOrgFile("Org_Vals_1.dat");
+  world.SetupOrgFile("Org_Vals_1.dat");
   
 
   emp::Ptr<Organism> new_org = new Organism(&random, 0.5);
