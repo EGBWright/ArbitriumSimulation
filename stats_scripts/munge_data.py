f298101f039ffc573a19644e06bb52a16ccb3641
ericwright81 <wrighte3@carleton.edu>
2021-03-03 18:26:03 +0000

    another small fix

--- stats_scripts/munge_data.py
+++ stats_scripts/munge_data.py
@@ -6,7 +6,7 @@ folder = ''
 #periods_resets = [[25,4000],[50,2000],[100,1000],[200,500],[400,250],[1000,100]]
 periods = ["25","50","100","200","400","1000"]
 seeds = range(10, 21)
-header = "uid treatment rep update coop count hist_0\n"
+header = "uid period seed update bacteria_pop phage_pop lysogen_pop arbitrium mean_lysogeny_rate mean_threshold evolver_pct\n"
 
 outputFileName = "munged_basic.dat"
 
@@ -21,9 +21,7 @@ for t in periods:
         for line in curFile:
             if (line[0] != "u"):
                 splitline = line.split(',')
-                outstring1 = "{} {} {} {} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], splitline[1], splitline[2], \
-                splitline[3],splitline[4], splitline[5], \
-                splitline[6])
+                outstring1 = "{} {} {} {} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], splitline[1], splitline[2], splitline[3],splitline[4], splitline[5], splitline[6], splitline[7])
                 outFile.write(outstring1)
         curFile.close()
 outFile.close()
